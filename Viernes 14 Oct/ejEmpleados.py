db9065a09e4bcf7021a59646dbbb3f0c1f01b828
Jorge Adolfo Ramírez Uresti <juresti@tec.mx>
2022-10-18 09:57:56 -0500

    Parte de archivos

--- Viernes 14 Oct/ejEmpleados.py
+++ Viernes 14 Oct/ejEmpleados.py
@@ -9,12 +9,40 @@ def menu():
     return int(input("Dime tu opcion: "))
 
 def leeDatosArchivo():
+    with open("empleados.txt","r") as miArchivo:
+        datosSucios = miArchivo.readlines()
+    print(datosSucios)
+    
+    #Quitar \n
+    for pos in range(len(datosSucios)):
+        datosSucios[pos] = datosSucios[pos].rstrip()
+    print(datosSucios)
+    
+    #Convertir en listas los strings
+    listaDatos = []
+    for empleado in datosSucios:
+        listaDatos.append(empleado.split(","))
+    print(listaDatos)
+    
+    for empleado in listaDatos:
+        empleado[1] = int(empleado[1])
+        empleado[2] = float(empleado[2])
+    print(listaDatos)
     print("Datos leidos del archivo")
-    return [["Jair",28,15.5],["Marisol",45,5.3],["Mario",7,99.9]]
+    return listaDatos #[["Jair",28,15.5],["Marisol",45,5.3],["Mario",7,99.9]]
 
 def escribeDatosArchivo(listaEmp):
     print("Se escribiran al archivo los siguientes datos:")
     print(listaEmp)
+    for empleado in listaEmp:
+        empleado[1] = str(empleado[1])
+        empleado[2] = str(empleado[2])
+    print(listaEmp)
+    
+    for pos in range(len(listaEmp)):
+        listaEmp[pos] = ",".join(listaEmp[pos])
+    print(listaEmp)
+    
     print("Se escribio el archivo")
 
 def reporteEmpleados(listaEmp):
